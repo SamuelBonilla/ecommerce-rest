aff7d2393a1cc9b8d0fbb136fd03c358eb198619
samuel bonilla <pythonners@gmail.com>
2016-06-15 01:31:00 +0000

    Relaciones y persos

--- common/models/producto.js
+++ common/models/producto.js
@@ -1,3 +1,21 @@
-module.exports = function(Producto) {
 
+var loopback = require('loopback');
+
+module.exports = function(Producto) {
+    
+    Producto.observe('before save', function(ctx, next) {
+            //SI CREO
+        if (ctx.instance) {
+            var ctxs = loopback.getCurrentContext();
+            var currentUser = ctxs.get('currentUser');
+            ctx.instance.fecha = new Date();
+            //ctx.instance.id_usuario = ctxs.active.accessToken["userId"];
+            console.log('currentUser.username: ', ctxs.active.accessToken);
+            //SI UPDATEO
+        } else {
+
+        }
+        //CONTINUO
+        next();
+    });
 };
--- common/models/valoracion.js
+++ common/models/valoracion.js
@@ -1,3 +1,15 @@
 module.exports = function(Valoracion) {
+    
+    Valoracion.observe('before save', function(ctx, next) {
+            //SI CREO
+        if (ctx.instance) {
+            ctx.instance.fecha = new Date();
+            //SI UPDATEO
+        } else {
+
+        }
+        //CONTINUO
+        next();
+    });
 
 };
